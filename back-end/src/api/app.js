6cfbbb4e295afba27439fb7dd305e5a829dd436a
GiovannaMorais <gigimorais9@hotmail.com>
2022-12-06 15:53:31 -0300

    Feat: create get Allproducts

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const loginRoutes = require('../database/routes/loginRoutes');
 const registerRoutes = require('../database/routes/registerRoutes');
+const productsRoutes = require('../database/routes/productsRoutes');
 
 const app = express();
 app.use(cors());
@@ -11,5 +12,6 @@ app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRoutes);
 app.use('/register', registerRoutes);
+app.use('/customer/products', productsRoutes);
 
 module.exports = app;
