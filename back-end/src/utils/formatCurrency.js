b13f05796d717bb15a8fecd65178bfd42594e09d
GiovannaMorais <gigimorais9@hotmail.com>
2022-12-08 19:14:43 -0300

    Fix: erro lint

--- back-end/src/utils/formatCurrency.js
+++ back-end/src/utils/formatCurrency.js
@@ -1,8 +1,6 @@
-function formatCurrency(value) {
-  return new Intl.NumberFormat('pt-br', {
+const formatCurrency = (value) => new Intl.NumberFormat('pt-br', {
     style: 'currency',
     currency: 'BRL',
   }).format(value);
-}
 
 module.exports = { formatCurrency };
