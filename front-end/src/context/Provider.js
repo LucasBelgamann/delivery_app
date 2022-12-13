c7a1d580b906015496b07e2d1f9254d7a826c730
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-12-13 15:32:15 -0300

    feat: complete page products

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -4,13 +4,15 @@ import Context from './context';
 
 function ThisProvider({ children }) {
   const [products, setProducts] = useState([]);
-  const contextValue = useMemo(() => ({ products, setProducts }), [products]);
+  const [counter, setCounter] = useState([]);
+  const [qty, setQty] = useState(0);
 
-  return (
-    <Context.Provider value={ contextValue }>
-      { children }
-    </Context.Provider>
+  const contextValue = useMemo(
+    () => ({ products, setProducts, counter, setCounter, qty, setQty }),
+    [counter, products, qty],
   );
+
+  return <Context.Provider value={ contextValue }>{children}</Context.Provider>;
 }
 
 ThisProvider.propTypes = { children: PropTypes.node.isRequired };
