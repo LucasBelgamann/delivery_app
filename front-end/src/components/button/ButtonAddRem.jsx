51333323bc6bc4010a4f7ffd755eb24a764523b6
LucasBelgamann <lucasbelgamann@hotmail.com>
2022-12-12 21:00:46 -0300

    feat: btns add and remove complete

--- front-end/src/components/button/ButtonAddRem.jsx
+++ front-end/src/components/button/ButtonAddRem.jsx
@@ -1,45 +1,38 @@
-// import { useState } from 'react';
-
-import { useContext } from 'react';
-import Context from '../../context/context';
+import { useState } from 'react';
 
 export default function ButtonAddRem() {
-  const { products } = useContext(Context);
-  //   const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  const handleAddToCart = (value) => {
-    // setCounter(Number(counter + 1));
-    console.log(value);
+  const handleAddToCart = () => {
+    setCounter(Number(counter + 1));
   };
 
-  const handleDecrementCartItem = (value) => {
-    // if (counter < 1) {
-    //   setCounter(0);
-    // } else {
-    //   setCounter(Number(counter - 1));
-    // }
-    console.log(value);
+  const handleDecrementCartItem = () => {
+    if (counter < 1) {
+      setCounter(0);
+    } else {
+      setCounter(Number(counter - 1));
+    }
   };
 
   return (
     <div>
-      {products.map((e) => (
-        <div className="btn-moreLess" key={ e.name }>
-          <button
-            type="button"
-            onClick={ () => handleDecrementCartItem(e) }
-          >
-            -
-          </button>
-          <p>0</p>
-          <button
-            type="button"
-            onClick={ () => handleAddToCart(e) }
-          >
-            +
-          </button>
-        </div>
-      ))}
+
+      <div className="btn-moreLess">
+        <button
+          type="button"
+          onClick={ handleDecrementCartItem }
+        >
+          -
+        </button>
+        <p>{counter}</p>
+        <button
+          type="button"
+          onClick={ handleAddToCart }
+        >
+          +
+        </button>
+      </div>
     </div>
 
   );
