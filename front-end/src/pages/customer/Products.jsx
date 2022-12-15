4861924e0dade6e96259288f56e2987b4b9ed702
GiovannaMorais <gigimorais9@hotmail.com>
2022-12-15 12:22:22 -0300

    Fix: errors in Products and Checkout Components

--- front-end/src/pages/customer/Products.jsx
+++ front-end/src/pages/customer/Products.jsx
@@ -4,9 +4,11 @@ import apiLogin from '../../utils/api';
 import Navbar from '../../components/navbar';
 import CardP from '../../components/CardP';
 import Context from '../../context/context';
+import { formatCurrency } from '../../utils/formatCurrency';
 
 function Products() {
   const { products, setProducts, cartItems, setCartItems } = useContext(Context);
+  // const [isDisable, setIsDisable] = useState(true);
   const history = useHistory();
 
   const setLocalStorage = (value) => {
@@ -14,18 +16,13 @@ function Products() {
     setCartItems(value);
   };
 
-  // const getLocalStorage = () => {
-  //   const local = localStorage.getItem('cart');
-  //   setReslocal(JSON.parse(local));
-  // };
-
   useEffect(() => {
     const getResponse = async () => {
       const { data } = await apiLogin.get('/customer/products');
       setProducts(data);
     };
     getResponse();
-  }, []);
+  }, [setProducts]);
 
   const addQuantity = ({ id, name, price, urlImage, quantity }) => {
     const indexItem = cartItems.findIndex((item) => item.id === id);
@@ -49,49 +46,81 @@ function Products() {
     }
   };
 
-  const setItem = ({ id, name, price, urlImage, quantity }) => {
+  const setQuantityWithInput = ({
+    id,
+    name,
+    price,
+    url_image: urlImage,
+    quantity,
+  }) => {
+    console.log('{ id, name, price, urlImage, quantity }', {
+      id,
+      name,
+      price,
+      urlImage,
+      quantity,
+    });
+    console.log('quantity', quantity);
     const indexItem = cartItems.findIndex((item) => item.id === id);
+    console.log('indexItem', indexItem);
     if (indexItem >= 0 && quantity > 0) {
       cartItems[indexItem].quantity = quantity;
+      console.log('setCartItemsPRIMEIRO', cartItems[indexItem].quantity);
       setLocalStorage([...cartItems]);
     }
     if (indexItem >= 0 && quantity <= 0) {
       cartItems.splice(indexItem, 1);
+      console.log('setCartItemsSEGUNDO', cartItems);
       setLocalStorage([...cartItems]);
     }
-    return setLocalStorage([...cartItems, { id, name, price, urlImage, quantity }]);
+    return setLocalStorage([
+      ...cartItems,
+      { id, name, price, urlImage, quantity },
+    ]);
+  };
+
+  const quantity = (product) => {
+    const item = cartItems.find((e) => e.id === product);
+    return item?.quantity;
   };
 
   return (
     <>
       <Navbar />
       <div className="products-container">
-        {
-          products.map((product) => (
-            <CardP
-              key={ product.id }
-              id={ product.id }
-              price={ product.price }
-              img={ product.url_image }
-              name={ product.name }
-              addQuantity={ addQuantity }
-              removeQuantity={ removeQuantity }
-              setItem={ setItem }
-              qty={ cartItems.find((item) => item.id === product.id)?.quantity }
-            />
-          ))
-        }
+        {products.map(({ id, name, price, url_image: img }) => (
+          <CardP
+            key={ id }
+            objProducts={ {
+              id,
+              name,
+              price,
+              img,
+            } }
+            addQuantity={ addQuantity }
+            removeQuantity={ removeQuantity }
+            setQuantityWithInput={ setQuantityWithInput }
+            qty={ quantity(id) }
+          />
+        ))}
       </div>
       <button
         type="button"
         className="cart-total"
+        data-testid="customer_products__button-cart"
         onClick={ () => history.push('/customer/checkout') }
+        disabled={ cartItems.length === 0 }
       >
-        Ver carrinho:
-        {' '}
-        { cartItems.reduce((acc, i) => i.price * i.quantity + acc, 0).toFixed(2) }
-        {' '}
-        {' '}
+        <p data-testid="customer_products__checkout-bottom-value">
+          Ver carrinho:
+          {' '}
+          {formatCurrency(
+            cartItems
+              .reduce((acc, i) => i.price * i.quantity + acc, 0)
+              .toFixed(2),
+          )}
+          {' '}
+        </p>
       </button>
     </>
   );
