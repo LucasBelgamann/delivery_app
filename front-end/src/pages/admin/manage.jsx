17b92953e4d9f9aa988e63585067e9baaca10ce5
AntunesGean <gean.antunes@gmail.com>
2022-12-15 19:47:43 -0300

    feat: criação dos elementos na tela manage

--- front-end/src/pages/admin/manage.jsx
+++ front-end/src/pages/admin/manage.jsx
@@ -1,12 +1,124 @@
+import React, { useState } from 'react';
 import Navbar from '../../components/navbar';
+import SubmitBtnAdmin from '../../components/submitBtnAdmin';
+import { CSS } from '../login';
 
-function manage() {
+function Manage() {
+  const [loginInput, setLogin] = useState({
+    name: '',
+    email: '',
+    password: '',
+    role: '',
+  });
+
+  const [disabledBtn, setDisabledBtn] = useState(true);
+
+  const handleChange = ({ target: { name, value } }) => {
+    setLogin((oldState) => ({ ...oldState, [name]: value }));
+    const PASSMIN = 5;
+    const NAMEMIN = 12;
+    const regex = /\w+@[a-z]+.com/g;
+    if (loginInput.email.match(regex)
+      && loginInput.password.length >= PASSMIN
+      && loginInput.name.length >= NAMEMIN
+      && loginInput.role !== '') {
+      setDisabledBtn(false);
+    } else {
+      return setDisabledBtn(true);
+    }
+  };
+
+  const roles = [{
+    name: 'Escolha uma Opção',
+    value: '',
+  }, {
+    name: 'Administrator',
+    value: 'administrator',
+  }, {
+    name: 'Seller',
+    value: 'seller',
+  }, {
+    name: 'Customer',
+    value: 'customer',
+  }];
+
   return (
-    <div>
+    <section>
       <Navbar />
-      <h1>Admin Manage </h1>
-    </div>
+      <main style={ CSS.Section }>
+        <form style={ CSS.Form }>
+          <label htmlFor="name" style={ CSS.Label }>
+            Nome:
+            <input
+              id="name"
+              type="name"
+              data-testid="admin_manage__input-name"
+              value={ loginInput.name }
+              onChange={ handleChange }
+              name="name"
+              placeholder=" seu nome"
+              style={ CSS.Input }
+            />
+          </label>
+          <label htmlFor="email" style={ CSS.Label }>
+            Email:
+            <input
+              id="email"
+              type="email"
+              data-testid="admin_manage__input-email"
+              value={ loginInput.email }
+              onChange={ handleChange }
+              name="email"
+              placeholder=" seu-email@site.com.br"
+              style={ CSS.Input }
+            />
+          </label>
+          <label htmlFor="password" style={ CSS.Label }>
+            Senha:
+            <input
+              id="password"
+              type="password"
+              data-testid="admin_manage__input-password"
+              value={ loginInput.password }
+              onChange={ handleChange }
+              name="password"
+              placeholder=" ********"
+              style={ CSS.Input }
+            />
+          </label>
+          <label htmlFor="tipo" style={ CSS.Label }>
+            Tipo:
+            <select
+              id="tipo"
+              style={ CSS.Input }
+              name="role"
+              onChange={ handleChange }
+              data-testid="admin_manage__select-role"
+            >
+              {roles.map((role) => (
+                <option
+                  key={ role.value }
+                  value={ role.value }
+                  data-testid={ `common_register__input-tipo-${role.value}` }
+                >
+                  { role.name }
+                </option>
+              ))}
+            </select>
+          </label>
+          <SubmitBtnAdmin
+            dataTestid="admin_manage__button-register"
+            routeSuffix="register"
+            sendObject={ loginInput }
+            navigation="/admin/manage"
+            btnName="CADASTRAR"
+            disabledBtn={ disabledBtn }
+          />
+          <p data-testid="common_register__element-invalid_register" />
+        </form>
+      </main>
+    </section>
   );
 }
 
-export default manage;
+export default Manage;
